94ae50f3a35586e402253a9e9d011ff0840d9022
Bhargavspeaks <99461820+RepalleBhargav@users.noreply.github.com>
2022-04-14 11:47:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,29 @@
 provider "aws" {
-  region = var.aws_region
+  region = "us-west-2"
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
+resource "random_id" "my-random-id" {
+  byte_length = 2
 }
 
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
-    Name = var.tag_name
-  }
-}
+resource "aws_s3_bucket" "my-test-bucket" {
+  bucket = "${var.s3_bucket_name}-${random_id.my-random-id.dec}"
+  acl    = "private"
 
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      storage_class = "STANDARD_IA"
+      days          = 30
+    }
+  }
+
+  tags = {
+    Name = "21-days-of-aws-using-terraform"
   }
 }
